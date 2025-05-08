3677c0b93b843099c03c5f916115923cac92e6c4
VellerRider <geadro@outlook.com>
2025-05-08 18:48:10 -0400

    small fix

--- public/js/components.js
+++ public/js/components.js
@@ -19,7 +19,7 @@ export const components = {
         return `
             <div class="current-reading-card" data-book-id="${book.book_id}">
                 <div class="current-reading-cover">
-                    <img src="${book.book_image}" alt="${book.book_title}" loading="lazy">
+                    <img src="${book.book_image}" alt="${book.book_title}">
                 </div>
                 <div class="current-reading-title">${book.book_title}</div>
                 <div class="current-reading-author">${book.authors}</div>
